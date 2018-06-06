39822fb7c8e049216d3f252d5fffb197802651b7
Laura <laurabeltran91_3@hotmail.com>
2018-06-06 14:25:55 -0500

    varibles

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+//Instrucciones Triviagame
+
+//Preguntas Random que queremos preguntar, así  como sus distintas opciones de respuesta.
+
+var question = ["What is the name of the third Harry Potter book?", "What is the last name of Harry Potter's uncles?", 
+"What is the name of the four houses of Hogwarts?", "What animal is not permitted for the students in Howarts?"];
+
+var answer = ["Harry Potter and the Prisoner of Azkaban", "Dursley", "Hufflepuff,Ravenclaw,Gryffindor,Slytherin","dog"];
+
+var firstAnswerGroup = ["Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Chamber of Secrets","Harry Potter and the Goblet of Fire","Harry Potter and the Half-Blood Prince"];
+var secondAnswerGroup = ["Weasley", "Dursley", "Granger", "Longbottom"];
+var thirdAnswerGroup = ["Hufflepuff,Ravenclaw,Gryffindor,Longbottom","Hufflepuff,Ravenclaw,CrossRoad,Slytherin","Hufflepuff,Ravenclaw,Gryffindor,Slytherin","Hufflepuff,Toadclaw,Gryffindor,Slytherin"];
+var fourthAnswerGroup = ["dog", "cat", "rat", "owl"]
+
+//Mostrar el tiempo que se tiene para contestar cada pregunta (15seg)
+
+//Si la respuesta que selecciono es correcta, te debe mostrar respuesta correcta, en otra pagina y los segundos restantes
+
+//Si la respuesta es incorrecta, te debe mostrar respuesta incorrecta y la solución correcta, además de los segundos restantes.
+
+//Cuando llegues al final de la trivia, te debe mostrar respuestas correctas, incorrectas y sin responder. Además de Start Over?
+
+
+
